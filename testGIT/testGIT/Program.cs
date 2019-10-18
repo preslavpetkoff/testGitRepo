cae4269aab73fc7c54deb00c5939b5a5642b76ba
preslav.petkov <preslav.petkov@inensia.com>
2019-10-18 12:00:36 +0300

    creating the project; implementing user interface engineer first task;
    Signed-off-by: preslav.petkov <preslav.petkov@inensia.com>

---
+++ testGIT/testGIT/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace testGIT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ReadLine();
+            Console.WriteLine("I'm the user interface developer!");
+        }
+    }
+}
